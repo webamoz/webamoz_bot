d2944291522fef23d9f8d0f37abce8a429bf0fbf
webamoz <mahnaz.9131@gmail.com>
2016-01-28 13:14:01 -0800

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -220,7 +220,7 @@ function create_config( )
     "lock_link",
     "all"
     },
-    sudo_users = {120518968,132472033},--Sudo users
+    sudo_users = {169866877},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
